01b92ffcfb47b54b2f2d9a0770e4c540cfb5c858
Yeri <yeri@accelbyte.net>
2021-04-09 08:23:09 +0700

    path parameter

--- app/main.py
+++ app/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"message": "Hello fastapi"}
+
+@app.get("/users/me")
+def get_me():
+    return {"user": "current user"}
+
+@app.get("/users/{user_id}")
+def get_users(user_id: int):
+    return {"user": f"User with the id {user_id}"}
